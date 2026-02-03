c5cba942ce9a17a850d4cb7278d7bd6248dac51c
Paloma Wilson <wilsonp26@up.edu>
2026-02-02 22:59:50 -0800

    Switch and Candles

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -2,6 +2,8 @@ package cs301.birthdaycake;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goodbye(View button) {
+        Log.i("button", "Goodbye");
+        finishAffinity();
     }
 }
